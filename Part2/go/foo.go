10d61d32f4d5d48b867e838d7715a8b07811ec33
sigtot <sigurdtotland@gmail.com>
2019-01-22 22:51:35 +0100

    Do go task in part 2

--- Part2/go/foo.go
+++ Part2/go/foo.go
@@ -1,9 +1,7 @@
-// Use `go run foo.go` to run your program
-
 package main
 
 import (
-    . "fmt"
+    "fmt"
     "runtime"
 )
 
@@ -13,43 +11,54 @@ const (
 	Exit
 )
 
-func number_server(add_number <-chan int, control <-chan int, number chan<- int) {
-	var i = 0
+func numberServer(addNumber <-chan int, control <-chan int, number chan<- int) {
+    i := 0
 
 	// This for-select pattern is one you will become familiar with if you're using go "correctly".
 	for {
 		select {
-			// TODO: receive different messages and handle them correctly
-			// You will at least need to update the number and handle control signals.
+		case n := <-addNumber:
+		    i += n
+        case c := <-control:
+            switch c {
+            case GetNumber:
+                number<- i
+            case Exit:
+                return
+            }
 		}
 	}
 }
 
-func incrementing(add_number chan<-int, finished chan<- bool) {
+func incrementing(addNumber chan<-int, finished chan<- bool) {
 	for j := 0; j<1000000; j++ {
-		add_number <- 1
+		addNumber <- 1
 	}
-	//TODO: signal that the goroutine is finished
+	finished <- true
 }
 
-func decrementing(add_number chan<- int, finished chan<- bool) {
+func decrementing(addNumber chan<- int, finished chan<- bool) {
 	for j := 0; j<1000000; j++ {
-		add_number <- -1
+		addNumber <- -1
 	}
-	//TODO: signal that the goroutine is finished
+    finished<- true
 }
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
+    addNumber := make(chan int)
+    control := make(chan int)
+    number := make(chan int)
+    finished := make(chan bool)
 
-	// TODO: Construct the required channels
-	// Think about wether the receptions of the number should be unbuffered, or buffered with a fixed queue size.
+    go numberServer(addNumber, control, number)
 
-	// TODO: Spawn the required goroutines
-
-	// TODO: block on finished from both "worker" goroutines
+    go incrementing(addNumber, finished)
+    go decrementing(addNumber, finished)
+    <-finished
+    <-finished
 
 	control<-GetNumber
-	Println("The magic number is:", <- number)
+	fmt.Println("The magic number is:", <- number)
 	control<-Exit
 }
